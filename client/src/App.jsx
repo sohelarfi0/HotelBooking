3a9e35c322eb340804584f54d7b666c5a1d7bc40
sohel <sohelarfi0@gmail.com>
2026-03-11 23:58:23 +0530

    implement the hero , login sections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
-import { useLocation } from 'react-router-dom'
-
+import {Route,Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home.jsx'
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -9,6 +9,14 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+
+
+      </div>
+
     </div>
   )
 }
