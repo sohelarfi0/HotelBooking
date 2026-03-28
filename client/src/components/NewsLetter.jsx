4b421ebf45a9d6d5e50a5e313fb911ef9e226af2
sohel <sohelarfi0@gmail.com>
2026-03-28 12:14:07 +0530

    implement hotel registration , newsletter, hotelowners folders like navbar, add room, list room, layout, dashboard for owner to mantain his hotel easily

--- client/src/components/NewsLetter.jsx
+++ client/src/components/NewsLetter.jsx
@@ -5,28 +5,28 @@ import { Title } from './Title'
 const NewsLetter = () => {
   return (
     
-    <div class="flex flex-col items-center
-     max-w-5xl lg:w-full rounded-2xl px-4 py-12 md:py-16 mx-2 lg:mx-auto my-30 bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 border border-gray-100 shadow-lg">
+    <div className="flex flex-col items-center
+     max-w-5xl lg:w-full rounded-2xl px-4 py-12 md:py-16 mx-2 lg:mx-auto my-30 bg-gradient from-blue-50 via-indigo-50 to-purple-50 border border-gray-100 shadow-lg">
     
     <Title title="Stay Ahead of the Journey" subTitle="Join our exclusive newsletter and unlock a world of premium travel experiences. Be the first to know about hidden gems, exclusive deals, and personalized recommendations tailored just for you." />
      
-    <div class="flex flex-col justify-center items-center text-center">
+    <div className="flex flex-col justify-center items-center text-center">
     
-    <div class="flex flex-col md:flex-row items-center justify-center
+    <div className="flex flex-col md:flex-row items-center justify-center
      gap-4 mt-6">
 
 
-        <input type="text" class="bg-white/80 backdrop-blur-sm px-4 py-2.5 border border-gray-200 rounded-lg 
+        <input type="text" className="bg-white/80 backdrop-blur-sm px-4 py-2.5 border border-gray-200 rounded-lg 
         outline-none max-w-66 w-full shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="Enter your email"/>
-            <button class="flex items-center justify-center gap-2 
-            group bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700 px-4 md:px-7 py-2.5 rounded-lg active:scale-95 transition-all shadow-md text-white">
+            <button className="flex items-center justify-center gap-2 
+            group bg-gradien-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700 px-4 md:px-7 py-2.5 rounded-lg active:scale-95 transition-all shadow-md text-black font-medium">
             Subscribe
             <img src={assets.arrowIcon} alt="arrow-icon" className='w-3.5 invert 
             group-hover:translate-x-1 transition-all' />
             
             </button>
     </div>
-    <p class="text-gray-600 mt-6 text-xs
+    <p className="text-gray-600 mt-6 text-xs
      text-center">By subscribing, you agree to our Privacy
       Policy and consent to receive updates.</p>
 
---
+++ client/src/pages/HotelOwner/DashBoard.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { assets, dashboardDummyData } from '../../assets/assets'
+import { Title } from '../../components/Title';
+const DashBoard = () => {
+    const [dashboardData, setDasboardData] = useState(dashboardDummyData);
+
+  return (
+    <div >
+        <Title align="left" font='outfit' title='DashBoard'
+        subTitle='Monitor your room listings, track bookings and analyze revenue-all in one place. Stay updated with real-time insights to ensure smoot operations.' />
+        <div className='flex gap-4 my-8'>
+            <div className='bg-primary/3 border border-primary/10 
+            rounded flex p-4 pr-8'>
+                <img src={assets.totalBookingIcon} alt="" className='max-sm:hidden h-10' />
+                <div className='flex flex-col sm:ml-4 font-medium'>
+                    <p className='text-blue-500 text-lg'>Total Bookings</p>
+                    <p className='text-neutral-400 text-base'>{dashboardData.totalBookings}</p>
+
+                </div>
+
+            
+                </div>
+                <div className='bg-primary/3 border border-primary/10 
+            rounded flex p-4 pr-8'>
+                <img src={assets.totalRevenueIcon} alt="" className='max-sm:hidden h-10' />
+                <div className='flex flex-col sm:ml-4 font-medium'>
+                    <p className='text-blue-500 text-lg'>Total Revenue</p>
+                    <p className='text-neutral-400 text-base'>{dashboardData.totalRevenue}</p>
+
+                </div>
+
+
+            </div>
+        </div>
+        <h2 className='text-xl text-blue-900/70 font-medium mb-5'>Recent Bookings</h2>
+
+        <div className='w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll'>
+            <table className='w-full'>
+                <thead className='bg-gray-50'>
+                    <tr>
+                        <th className='py-3 px-4 text-gray-800 font-medium'>User Name</th>
+                        <th className='py-3 px-4 text-gray-800 font-medium max-sm:hidden'> Room Name</th>
+                        <th className='py-3 px-4 text-gray-800 font-medium text-center'>Total Amount</th>
+                        <th className='py-3 px-4 text-gray-800 font-medium text-center'>Payment Status</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+
+    </div>
+  )
+}
+
+export default DashBoard
