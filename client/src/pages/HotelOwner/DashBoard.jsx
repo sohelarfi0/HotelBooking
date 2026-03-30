ab2fedbd5a51f52fec6a676139c88bd9d56ad694
sohel <sohelarfi0@gmail.com>
2026-03-30 22:59:03 +0530

    implemented the Addroom, Dashboard, listRooms options for the owners of the hotels

--- client/src/pages/HotelOwner/DashBoard.jsx
+++ client/src/pages/HotelOwner/DashBoard.jsx
@@ -44,6 +44,30 @@ const DashBoard = () => {
                         <th className='py-3 px-4 text-gray-800 font-medium text-center'>Payment Status</th>
                     </tr>
                 </thead>
+                <tbody className='text-sm'>
+                    {dashboardData.bookings.map((item, index)=>(
+                        <tr key={index}>
+                            <td className='py-3 px-4 text-gray-700 border-t border-gray-300 '>
+                                {item.user.username}
+                            </td>
+                            <td className='py-3 px-4 text-gray-700 border-t border-gray-300  max-sm:hidden'>
+                                {item.room.roomType}
+                            </td>
+                            <td className='py-3 px-4 text-gray-700 border-t border-gray-300  text-center'>
+                                ${item.totalPrice}
+                            </td>
+                            <td className='py-3 px-4 border-t border-gray-300 flex'>
+                                <button className={`py-1 px-3 text-xs rounded-full mx-auto 
+                                    ${item.isPaid ? 'bg-green-200 text-green-600' : 'bg-amber-200 text-yellow-600'}`}>
+                                        {item.isPaid ? 'Completed' : 'Pending'}
+                                    </button>
+
+                            </td>
+
+                        </tr>
+                    ))}
+
+                </tbody>
             </table>
         </div>
 
--- client/src/pages/HotelOwner/ListRoom.jsx
+++ client/src/pages/HotelOwner/ListRoom.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { roomsDummyData } from '../../assets/assets'
+import { Title } from '../../components/Title';
 
 const ListRoom = () => {
+
+  const [rooms, setRooms] =useState(roomsDummyData);
+
   return (
-    <div>ListRoom</div>
+    <div>
+      <Title align='left' font='outfit' title='Room Listings' subTitle='View, edit or manage all listed rooms.
+       keep the information up-to-date to provide the best experience for users.' />
+        <p className=' text-gray-500 mt-8'>All Rooms </p>
+        <div className='w-full max-w-3xl text-left border border-gray-300 rounded-lg
+        max-h-80 overflow-y-scroll'>
+          <table className='w-full '>
+            <thead className='bg-gray-50'>
+                    <tr>
+                        <th className='py-3 px-4 text-gray-800 font-medium'>Name</th>
+                        <th className='py-3 px-4 text-gray-800 font-medium max-sm:hidden'> Facility</th>
+                        <th className='py-3 px-4 text-gray-800 font-medium text-center'>Price / night</th>
+                        <th className='py-3 px-4 text-gray-800 font-medium text-center'>Actions</th>
+                    </tr>
+                </thead>
+                <tbody className='text-sm'>
+                  {
+                    rooms.map((item, index)=>(
+                      <tr key={index}>
+                        <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>
+                          {item.roomType}
+                        </td>
+                        <td className='py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden
+                        max:hidden'>
+                          {item.amenities.join(', ')}
+                        </td>
+                        <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-center'>
+                          ${item.pricePerNight.toFixed(2)}
+                        </td>
+                        <td className='py-3 px-4  border-t border-gray-300 text-sm text-red-500 text-center '>
+                          <label className='relative inline-flex items-center cursor-pointertext-gray-900 gap-3'>
+                            <input type="checkbox" className='sr-only peer' checked={item.isAvailable} onChange={() => setRooms(rooms.map((room, i) => i === index ? { ...room, isAvailable: !room.isAvailable } : room))}/>
+                            <div className='w-12 h-7 bg-slate-300 rounded-full peer
+                            peer-checked:bg-blue-600 transition-colors duration-200'>
+
+                            </div>
+                            <span className='dot absolute left-1 top-1 w-5 h-5 bg-white 
+                            rounded-full transition-transform duration-200 ease-in=out
+                            peer-checked:translate-x-5'></span>
+                          </label>
+                        </td>
+                    
+                      
+            
+                      </tr>
+
+                    ))
+
+                  }
+                </tbody>
+          </table>
+        </div>
+    </div>
   )
 }
 
